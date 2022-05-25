d45e0d23d72c551c5cabb649573b2d89d90c8fef
Volodymyr Kozyr <vroger4@gmail.com>
2022-05-25 18:39:51 +0300

    homework#9
    task 1 and 2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,83 +11,103 @@ const refs = {
     restHours: document.querySelector('span[data-hours]'),
     restMinutes: document.querySelector('span[data-minutes]'),
     restSeconds: document.querySelector('span[data-seconds]'),
-    // timer: document.querySelector('.timer'),
-    // field: document.querySelector('.field'),
+    timer: document.querySelector('.timer'),
+    fields: document.querySelectorAll('.field'),
+    values: document.querySelectorAll('.value'),
 };
-// console.log(refs.timer);
-// refs.timer.style.color = 'tomato';
-// refs.timer.style.fontSize = '22px';
-// refs.timer.style.cssText  = 'display:flex';
-// refs.field.style.width = '40px';
-// refs.field.style.height = '120px';
-// refs.field.style.backgroundColor = 'blue';
-// refs.field.style.color = 'white';
+
+refs.timer.style.display = 'flex';
+
+for (const field of refs.fields) {
+  field.style.width = '65px';
+  field.style.height = '65px';
+  field.style.backgroundColor = 'blue';
+  field.style.color = 'white';
+  field.style.textAlign = 'center';
+  field.style.marginRight = '4px';
+  field.style.marginTop = '4px';
+  field.style.paddingTop = '4px';
+  field.style.paddingRight = '4px';
+  field.style.paddingBottom = '4px';
+  field.style.paddingLeft = '4px';
+  field.style.borderRadius = '4px';
+};  
+
+for (const value of refs.values) {
+  value.style.marginRight = '4px';
+}
+
+let intervalId = null;
+let selectedDate = '';
 
 refs.startBtn.disabled = true;
-let intervalId = null;
-
-let selectedDate = null;
+refs.startBtn.addEventListener('click', handleStartBtn);
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        if ((selectedDates[0] - Date.now()) < 0) {
-            Notify.failure('Please choose a date in the future');
-            refs.startBtn.disabled = true;
-            return;
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0].getTime();
 
-        } else {
-            refs.startBtn.disabled = false;
-            refs.startBtn.addEventListener('click', handleStartBtn);
+    if ((selectedDate - options.defaultDate) < 0) {
+      Notify.failure('Please choose a date in the future');
+      refs.startBtn.disabled = true;
+      return;
+    };
 
-            function handleStartBtn() {
-                intervalId = setInterval(() => {
-                    const timeLeftMs = selectedDates[0] - Date.now();
-                    const { days, hours, minutes, seconds } = convertMs(timeLeftMs);
-                    refs.restDays.textContent = `${days}`;
-                    refs.restHours.textContent = `${hours}`;
-                    refs.restMinutes.textContent = `${minutes}`;
-                  refs.restSeconds.textContent = `${seconds}`;
-                }, 1000);
-                
-                refs.startBtn.disabled = true;
-            }
-        }
-    },
+    refs.startBtn.disabled = false;
+    timerCount();
+  },
 };
 
 flatpickr("input", options);
+
+function timerCount() {
+  const currentTime = Date.now();
+  const timeLeftMs = selectedDate - currentTime;
+  const timeLeft = convertMs(timeLeftMs);
+
+  if (timeLeftMs > 0) {
+    updateTimer(timeLeft);
+  };
+
+  if (timeLeftMs < 1000) {
+  clearInterval(intervalId);
+  };
+};
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  refs.restDays.textContent = `${days}`;
+  refs.restHours.textContent = `${hours}`;
+  refs.restMinutes.textContent = `${minutes}`;
+  refs.restSeconds.textContent = `${seconds}`;
+};
+
+function handleStartBtn() {
+  if (intervalId) {
+    return;
+  };
+
+  intervalId = setInterval(timerCount, 1000);
+  refs.startBtn.disabled = true;
+};
 
 function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 };
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-  // Remaining days
-    const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-    return { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds };
 };
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-// const obd = {
-//   days:0, hours:0, minutes:0, seconds:0 
-// }
-// console.log(obd === 0);
